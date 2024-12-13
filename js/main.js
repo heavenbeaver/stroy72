4df88a05a92a961fca58d017f8aec8e9ca808ef3
heavenbeaver <daniilbobritskiy@outlook.com>
2024-12-14 01:38:38 +0500

    Почти готова первая версия

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let servicesSection = document.querySelector('.services');
+let serviceCard_1 = document.getElementById('sc-1');
+let serviceCard_2 = document.getElementById('sc-2');
+let serviceCard_3 = document.getElementById('sc-3');
+let serviceCard_4 = document.getElementById('sc-4');
+
+document.addEventListener('scroll', () => {
+    const rect_1 = serviceCard_1.getBoundingClientRect();
+    const rect_2 = serviceCard_2.getBoundingClientRect();
+    const rect_3 = serviceCard_3.getBoundingClientRect();
+    const rect_4 = serviceCard_4.getBoundingClientRect();
+    const rect = servicesSection.getBoundingClientRect();
+
+    const windowHeight = window.innerHeight;
+
+    if (rect_1.top < windowHeight * 0.75 && rect_1.bottom > windowHeight * 0.25) {
+        serviceCard_1.style.transform = `translateX(0)`;
+        serviceCard_1.style.opacity = 1;
+    }
+
+    if (rect_2.top < windowHeight * 0.75 && rect_2.bottom > windowHeight * 0.25) {
+        serviceCard_2.style.transform = `translateX(0)`;
+        serviceCard_2.style.opacity = 1;
+    }
+
+    if (rect_3.top < windowHeight * 0.75 && rect_3.bottom > windowHeight * 0.25) {
+        serviceCard_3.style.transform = `translateX(0)`;
+        serviceCard_3.style.opacity = 1;
+    }
+
+    if (rect_4.top < windowHeight * 0.75 && rect_4.bottom > windowHeight * 0.25) {
+        serviceCard_4.style.transform = `translateX(0)`;
+        serviceCard_4.style.opacity = 1;
+    }
+    // if (rect.top < windowHeight * 0.75 && rect.bottom > windowHeight * 0.25) {
+    //     serviceCard_1.style.transform = `translateX(0)`;
+    //     serviceCard_1.style.opacity = 1;
+    //     serviceCard_2.style.transform = `translateX(0)`;
+    //     serviceCard_2.style.opacity = 1;
+    //     serviceCard_3.style.transform = `translateX(0)`;
+    //     serviceCard_3.style.opacity = 1;
+    //     serviceCard_4.style.transform = `translateX(0)`;
+    //     serviceCard_4.style.opacity = 1;
+    // }
+})
---
+++ js/slider.js
@@ -0,0 +1,37 @@
+let slider = document.querySelector('.slider');
+let sliderTrack = document.querySelector('.slider__track');
+let slides = document.querySelectorAll('.slide');
+let slide = document.querySelector('.slide');
+// let sliderWrapper = document.querySelector('.slide__wrapper');
+let sliderBtnLeft = document.getElementById('sliderBtnLeft');
+let sliderBtnRight = document.getElementById('sliderBtnRight');
+let counterText = document.querySelector('.slides__counter');
+let counter = 1;
+let currentPosition = 0;
+let offset = slide.scrollWidth;
+let maxOffset = (slides.length - 1) * -offset;
+
+// начальное значение счетчика слайдера
+counterText.innerText = `${counter} из ${slides.length}`
+
+sliderBtnRight.addEventListener('click', () => {
+    if (currentPosition > maxOffset) {
+        currentPosition = currentPosition - offset;
+        sliderTrack.style.transform = `translateX(${currentPosition}px)`;
+        counter++;
+        counterText.innerText = `${counter} из ${slides.length}`
+    } else {
+        return
+    }
+});
+
+sliderBtnLeft.addEventListener('click', () => {
+    if (currentPosition != 0) {
+        currentPosition = currentPosition + offset;
+        sliderTrack.style.transform = `translateX(${currentPosition}px)`;
+        counter--;
+        counterText.innerText = `${counter} из ${slides.length}`
+    } else {
+        return
+    }
+});

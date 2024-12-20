d99f88b4aaa628d451c80ea32eb0bba274debbac
heavenbeaver <daniilbobritskiy@outlook.com>
2024-12-20 16:51:12 +0500

    Переделка Header

--- js/main.js
+++ js/main.js
@@ -4,65 +4,60 @@ let serviceCard_2 = document.getElementById('sc-2');
 let serviceCard_3 = document.getElementById('sc-3');
 let serviceCard_4 = document.getElementById('sc-4');
 
-document.addEventListener('scroll', () => {
-    const rect_1 = serviceCard_1.getBoundingClientRect();
-    const rect_2 = serviceCard_2.getBoundingClientRect();
-    const rect_3 = serviceCard_3.getBoundingClientRect();
-    const rect_4 = serviceCard_4.getBoundingClientRect();
-    const rect = servicesSection.getBoundingClientRect();
 
-    const windowHeight = window.innerHeight;
+// document.addEventListener('scroll', () => {
+//     const rect_1 = serviceCard_1.getBoundingClientRect();
+//     const rect_2 = serviceCard_2.getBoundingClientRect();
+//     const rect_3 = serviceCard_3.getBoundingClientRect();
+//     const rect_4 = serviceCard_4.getBoundingClientRect();
+//     const rect = servicesSection.getBoundingClientRect();
 
-    if (rect_1.top < windowHeight * 0.75 && rect_1.bottom > windowHeight * 0.25) {
-        serviceCard_1.style.transform = `translateX(0)`;
-        serviceCard_1.style.opacity = 1;
+//     const windowHeight = window.innerHeight;
+
+//     if (rect_1.top < windowHeight * 0.75 && rect_1.bottom > windowHeight * 0.25) {
+//         serviceCard_1.style.transform = `translateX(0)`;
+//         serviceCard_1.style.opacity = 1;
+//     }
+
+//     if (rect_2.top < windowHeight * 0.75 && rect_2.bottom > windowHeight * 0.25) {
+//         serviceCard_2.style.transform = `translateX(0)`;
+//         serviceCard_2.style.opacity = 1;
+//     }
+
+//     if (rect_3.top < windowHeight * 0.75 && rect_3.bottom > windowHeight * 0.25) {
+//         serviceCard_3.style.transform = `translateX(0)`;
+//         serviceCard_3.style.opacity = 1;
+//     }
+
+//     if (rect_4.top < windowHeight * 0.75 && rect_4.bottom > windowHeight * 0.25) {
+//         serviceCard_4.style.transform = `translateX(0)`;
+//         serviceCard_4.style.opacity = 1;
+//     }
+//     // if (rect.top < windowHeight * 0.75 && rect.bottom > windowHeight * 0.25) {
+//     //     serviceCard_1.style.transform = `translateX(0)`;
+//     //     serviceCard_1.style.opacity = 1;
+//     //     serviceCard_2.style.transform = `translateX(0)`;
+//     //     serviceCard_2.style.opacity = 1;
+//     //     serviceCard_3.style.transform = `translateX(0)`;
+//     //     serviceCard_3.style.opacity = 1;
+//     //     serviceCard_4.style.transform = `translateX(0)`;
+//     //     serviceCard_4.style.opacity = 1;
+//     // }
+// })
+
+const header = document.querySelector('.header');
+const headerBanner = document.querySelector('.header__banner');
+
+window.addEventListener('scroll', () => {
+    // проверяем на сколько прокрутился экран
+    if (window.scrollY >= headerBanner.scrollHeight) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
     }
-
-    if (rect_2.top < windowHeight * 0.75 && rect_2.bottom > windowHeight * 0.25) {
-        serviceCard_2.style.transform = `translateX(0)`;
-        serviceCard_2.style.opacity = 1;
-    }
-
-    if (rect_3.top < windowHeight * 0.75 && rect_3.bottom > windowHeight * 0.25) {
-        serviceCard_3.style.transform = `translateX(0)`;
-        serviceCard_3.style.opacity = 1;
-    }
-
-    if (rect_4.top < windowHeight * 0.75 && rect_4.bottom > windowHeight * 0.25) {
-        serviceCard_4.style.transform = `translateX(0)`;
-        serviceCard_4.style.opacity = 1;
-    }
-    // if (rect.top < windowHeight * 0.75 && rect.bottom > windowHeight * 0.25) {
-    //     serviceCard_1.style.transform = `translateX(0)`;
-    //     serviceCard_1.style.opacity = 1;
-    //     serviceCard_2.style.transform = `translateX(0)`;
-    //     serviceCard_2.style.opacity = 1;
-    //     serviceCard_3.style.transform = `translateX(0)`;
-    //     serviceCard_3.style.opacity = 1;
-    //     serviceCard_4.style.transform = `translateX(0)`;
-    //     serviceCard_4.style.opacity = 1;
-    // }
 })
 
-let moreBtn = document.querySelector('.more__projects__title');
-let moreContent = document.querySelector('.more__projects__content');
-
-moreBtn.addEventListener('click', () => {
-    if (!moreBtn.classList.contains('active')){
-        moreBtn.classList.add('active');
-        moreContent.style.display = 'flex';
-        setTimeout(function(){
-            moreContent.style.opacity = 1;
-            moreContent.style.height = '100%';
-        }, 100)
-    } else {
-        moreBtn.classList.remove('active');
-        moreContent.style.height = 0;
-        moreContent.style.opacity = 0;
-        setTimeout(function() {
-            moreContent.style.display = 'none';
-        }, 100)
-
-    }
-    
+const bannerArrow = document.querySelector('.banner__arrow');
+bannerArrow.addEventListener('click', () => {
+    document.querySelector('.about').scrollIntoView({behavior: 'smooth'});
 })
